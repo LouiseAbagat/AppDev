aef72295ed214753d10222c37c6d13d4530cb030
Louise Abagat <jjabagat@paterostechnologicalcollege.edu.ph>
2025-02-24 12:27:12 +1100

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -10,5 +10,6 @@ public class StudentNames {
         // Example:
         // System.out.println("Juan Cruz - BSIT 2C");
         // System.out.println("Stefany Domingo Tapia - BSIT 2D");
+        // System.out.printIn("Jazmine Louise J. Abagat - BSIT 2C");
     }
 }
